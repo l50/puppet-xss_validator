5ccd0c11ecdb81f1960b7a6b54ac07c76abfba61
Jayson Grace <jayson.e.grace@gmail.com>
2016-09-05 20:55:28 -0600

    Updated docs, fixed parameter placement, test case

--- README.md
+++ README.md
@@ -1,59 +1,62 @@
-discover
+puppet-xss_validator
 ===================
-      
-      
 [![License](http://img.shields.io/:license-mit-blue.svg)](http://doge.mit-license.org)
 
 #### Table of Contents
 
 1. [Overview](#overview)
-2. [Module Description - What the module does and why it is useful](#module-description)
+2. [Module Description - What the module does and why it is
+   useful](#module-description)
 3. [Setup - The basics of getting started with discover](#setup)
     * [Setup requirements](#setup-requirements)
-    * [Beginning with discover](#beginning-with-discover)
+    * [Installing xssValidator](#installing-xssvalidator)
 4. [Usage - Configuration options and additional functionality](#usage)
 5. [Limitations - OS compatibility, etc.](#limitations)
 6. [Development - Guide for contributing to the module](#development)
 
 ## Overview
 
-Used to install the Discover tool on Kali.
+Used to install the xssValidator Burp Extension on Kali Linux.
 
 ## Module Description
 
-This module will install Discover on a Kali machine. This may be useful for Systems Administrators or
-Penetration Testers.
+This module will install xssValidator on a Kali machine.
 
 ## Setup
 
 ### Setup Requirements
 
-The git puppet module is required for this module to work
-properly. Please add it to your Puppetfile like so:
+The following puppet modules are required for this module to work
+properly. Please add them to your Puppetfile like so:
 
 ```
 mod 'puppetlabs/git'
+mod 'puppetlabs/vcsrepo'
+mod 'maestrodev-wget'
+mod '3fs/phantomjs'
 ```
 
-### Beginning with discover
+### Installing xssValidator
 
-Simply specify the directory you wish Discover to be installed to as a parameter and run the module as you would any other.
+Simply specify the directory you wish xssValidator to be installed in as
+a parameter and run the module as you would any other.
 
 ## Usage
-Install the discover tool to /opt/discover:
+Install the xssValidator Burp extension in /opt/xss_validator:
 ```
-class { 'discover':
-  install_location => '/opt/discover',
+class { 'xss_validator':
+  install_location => '/opt/xss_validator',
 }
 ```
 
 ## Limitations
 
-This has been built to work with Kali only.
+This has been tested on Kali only.
 
 ## Development
 
-Feel free to fork this up and issue a pull request if you want to improve on the
+Feel free to fork this up and issue a pull request if you want to
+improve on the
 work that has already been done.
 
 ## Release Notes/Contributors/Etc
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,26 +2,25 @@ class xss_validator::params {
   case $::osfamily {
     'Debian': {
       $install_location='/opt/xss_validator'
+      $commons_codec_download_location='http://central.maven.org/maven2/commons-codec/commons-codec/1.6/commons-codec-1.6.jar'
+      $commons_logging_download_location='http://central.maven.org/maven2/commons-logging/commons-logging/1.1.3/commons-logging-1.1.3.jar'
+      $fluent_hc_download_location='http://central.maven.org/maven2/org/apache/httpcomponents/fluent-hc/4.3.6/fluent-hc-4.3.6.jar'
+      $httpclient_download_location='http://central.maven.org/maven2/org/apache/httpcomponents/httpclient/4.3.6/httpclient-4.3.6.jar'
+      $httpclient_cache_download_location='http://central.maven.org/maven2/org/apache/httpcomponents/httpclient-cache/4.3.6/httpclient-cache-4.3.6.jar'
+      $httpcore_download_location='http://central.maven.org/maven2/org/apache/httpcomponents/httpcore/4.3.3/httpcore-4.3.3.jar'
+      $httpmime_download_location='http://central.maven.org/maven2/org/apache/httpcomponents/httpmime/4.3.6/httpmime-4.3.6.jar'
+      $dependencies = [
+        $commons_codec_download_location,
+        $commons_logging_download_location,
+        $fluent_hc_download_location,
+        $httpclient_download_location,
+        $httpclient_cache_download_location,
+        $httpcore_download_location,
+        $httpmime_download_location,
+      ]
     }
-
     default: {
       fail("Unsupported OS family: ${::osfamily}")
     }
   }
-  $commons_codec_download_location='http://central.maven.org/maven2/commons-codec/commons-codec/1.6/commons-codec-1.6.jar'
-  $commons_logging_download_location='http://central.maven.org/maven2/commons-logging/commons-logging/1.1.3/commons-logging-1.1.3.jar'
-  $fluent_hc_download_location='http://central.maven.org/maven2/org/apache/httpcomponents/fluent-hc/4.3.6/fluent-hc-4.3.6.jar'
-  $httpclient_download_location='http://central.maven.org/maven2/org/apache/httpcomponents/httpclient/4.3.6/httpclient-4.3.6.jar'
-  $httpclient_cache_download_location='http://central.maven.org/maven2/org/apache/httpcomponents/httpclient-cache/4.3.6/httpclient-cache-4.3.6.jar'
-  $httpcore_download_location='http://central.maven.org/maven2/org/apache/httpcomponents/httpcore/4.3.3/httpcore-4.3.3.jar'
-  $httpmime_download_location='http://central.maven.org/maven2/org/apache/httpcomponents/httpmime/4.3.6/httpmime-4.3.6.jar'
-  $dependencies = [
-    $commons_codec_download_location,
-    $commons_logging_download_location,
-    $fluent_hc_download_location,
-    $httpclient_download_location,
-    $httpclient_cache_download_location,
-    $httpcore_download_location,
-    $httpmime_download_location,
-  ]
 }
