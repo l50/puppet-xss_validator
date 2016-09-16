fb0d580bc75e25583a0898afd59830b12cae3e24
Jayson Grace <jayson.e.grace@gmail.com>
2016-09-16 14:42:18 -0600

    Updated documentation and fixed indenting

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,18 +6,39 @@
 # Parameters
 # ----------
 #
-# * `install`
+# * `install_location`
 # The directory where you want to install xss_validator
 #
-# Variables
-# ----------
+# * `commons_codec_download_location`
+# The directory where you want to install the commons_codec jar file
 #
-# N/A
+# * `commons_logging_download_location`
+# The directory where you want to install the commons_logging jar file
+#
+# * `fluent_hc_download_location`
+# The directory where you want to install the fluent_hc jar file
+#
+# * `httpclient_download_location`
+# The directory where you want to install the httpclient jar file
+#
+# * `httpclient_cache_download_location`
+# The directory where you want to install the httpclient_cache jar file
+#
+# * `httpcore_download_location`
+# The directory where you want to install the httpcore jar file
+#
+# * `httpmime_download_location`
+# The directory where you want to install the httpmime jar file
+#
+# Requirements
+# --------
+#
+# puppetlabs-stdlib - https://github.com/puppetlabs/puppetlabs-stdlib
+# puppetlabs-vcsrepo - https://github.com/puppetlabs/puppetlabs-vcsrepo
+# phantomjs - https://github.com/3fs/puppet-phantomjs
 #
 # Examples
 # --------
-#
-# @example
 #
 #    class { 'xss_validator':
 #      install_location => '/opt/xss_validator',
@@ -26,12 +47,12 @@
 # Authors
 # -------
 #
-# l50 <jayson.e.grace@gmail.com>
+# Jayson Grace <jayson.e.grace@gmail.com>
 #
 # Copyright
 # ---------
 #
-# Copyright 2016 l50
+# Copyright 2016 Jayson Grace
 #
 class xss_validator(
   $install_location=$xss_validator::params::install_location,
@@ -45,42 +66,34 @@ class xss_validator(
   $dependencies=$xss_validator::params::dependencies,
 ) inherits xss_validator::params {
 
-  package { [
-    'ant',
-  ]:
-    ensure => installed,
-  }
-
   include stdlib
   include git
   include phantomjs
 
-  exec { "apt-update":
-    command => "/usr/bin/apt-get update"
+  package { 'ant':
+    ensure => 'installed',
   }
-
-  Exec["apt-update"] -> Package <| |>
 
   vcsrepo { $install_location:
     ensure   => present,
     provider => git,
     source   => 'git://github.com/l50/xssValidator.git',
     require  => Class['git'],
-    } ->
+  } ->
 
-    file { "$install_location/burp-extender/lib":
-      ensure => 'directory',
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0755',
-    } ->
+  file { "$install_location/burp-extender/lib":
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  } ->
 
-    wget::fetch { $dependencies:
-      destination => "$install_location/burp-extender/lib/",
-    } ->
+  wget::fetch { $dependencies:
+    destination => "$install_location/burp-extender/lib/",
+  } ->
 
-    exec { 'compile_xss-validator_jar':
-      command => '/usr/bin/ant',
-      cwd     => "$install_location/burp-extender/bin/burp",
-    }
+  exec { 'compile_xss-validator_jar':
+    command => '/usr/bin/ant',
+    cwd     => "$install_location/burp-extender/bin/burp",
+  }
 }
